e939fcf9e2a498dd8c0d441079d43e1cd7b2b5ee
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2026-06-06 12:31:38 +0530

    added api routes for Comment Model

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -25,14 +25,22 @@ namespace api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll() // IActionResult allows us to return different types of responses (e.g., Ok, NotFound, etc.)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var stocks = await _stockRepository.GetAllAsync(); 
             var stockDtos = stocks.Select(s => s.ToStockDto()); // toList for deferred execution
             return Ok(stockDtos);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id) // model binding to get the id from the route
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var stock = await _stockRepository.GetByIdAsync(id);
             if (stock == null)
             {
@@ -44,6 +52,10 @@ namespace api.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var stockModel = stockDto.ToStockFromCreateDto();
             await _stockRepository.CreateAsync(stockModel);
 
@@ -52,9 +64,13 @@ namespace api.Controllers
 
         // update is used ith firstofDefault to check if the stock exists before updating
         [HttpPut]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var stockModel = await _stockRepository.UpdateAsync(id, updateDto);
             if (stockModel == null)
             {
@@ -66,10 +82,13 @@ namespace api.Controllers
         }
 
         [HttpDelete]
-        [Route("{id}")]
-
+        [Route("{id:int}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
            var stockModel = await _stockRepository.DeleteAsync(id);
 
            if(stockModel == null)
@@ -78,7 +97,6 @@ namespace api.Controllers
            }
            return NoContent(); // 204 success code
         }
-
 
     }
 }
